fbb0ba0bf81056ee999f98e09dc33e17a3cb018c
Nafis Chonchol <nafisislam161@gmail.com>
2023-07-07 15:06:40 +0600

    use main event

--- resources/views/templates/invester/partials/plan.blade.php
+++ resources/views/templates/invester/partials/plan.blade.php
@@ -55,59 +55,52 @@
                 }
                 modal.modal("show");
             });
+
             $('.investModal').click(function() {
                 var isInvested = $(this).data("is-invested");
-                console.log(isInvested);
+                if(isInvested)
+                    return false;
+                var symbol = '{{ $general->cur_sym }}';
+                var currency = '{{ $general->cur_text }}';
+                $('.gateway-info').addClass('d-none');
                 var modal = $('#investModal');
-                if(!isInvested)
-                    modal.modal("show");
-            });
-        
-
-            // $('.investModal').click(function() {
-            //     var isInvested = $(this).data("isInvested");
-            //     console.log(isInvested);
-            //     var symbol = '{{ $general->cur_sym }}';
-            //     var currency = '{{ $general->cur_text }}';
-            //     $('.gateway-info').addClass('d-none');
-            //     var modal = $('#investModal');
-            //     var plan = $(this).data('plan');
+                var plan = $(this).data('plan');
                 
-            //     modal.find('[name=plan_id]').val(plan.id);
-            //     modal.find('.planName').text(plan.name);
-            //     let fixedAmount = parseFloat(plan.fixed_amount).toFixed(2);
-            //     let minimumAmount = parseFloat(plan.minimum).toFixed(2);
-            //     let maximumAmount = parseFloat(plan.maximum).toFixed(2);
-            //     let interestAmount = parseFloat(plan.interest);
+                modal.find('[name=plan_id]').val(plan.id);
+                modal.find('.planName').text(plan.name);
+                let fixedAmount = parseFloat(plan.fixed_amount).toFixed(2);
+                let minimumAmount = parseFloat(plan.minimum).toFixed(2);
+                let maximumAmount = parseFloat(plan.maximum).toFixed(2);
+                let interestAmount = parseFloat(plan.interest);
 
-            //     if (plan.fixed_amount > 0) {
-            //         modal.find('.investAmountRange').text(`Invest: ${symbol}${fixedAmount}`);
-            //         modal.find('[name=amount]').val(parseFloat(plan.fixed_amount).toFixed(2));
-            //         modal.find('[name=amount]').attr('readonly', true);
-            //     } else {
-            //         modal.find('.investAmountRange').text(
-            //             `Invest: ${symbol}${minimumAmount} - ${symbol}${maximumAmount}`);
-            //         modal.find('[name=amount]').val('');
-            //         modal.find('[name=amount]').removeAttr('readonly');
-            //     }
+                if (plan.fixed_amount > 0) {
+                    modal.find('.investAmountRange').text(`Invest: ${symbol}${fixedAmount}`);
+                    modal.find('[name=amount]').val(parseFloat(plan.fixed_amount).toFixed(2));
+                    modal.find('[name=amount]').attr('readonly', true);
+                } else {
+                    modal.find('.investAmountRange').text(
+                        `Invest: ${symbol}${minimumAmount} - ${symbol}${maximumAmount}`);
+                    modal.find('[name=amount]').val('');
+                    modal.find('[name=amount]').removeAttr('readonly');
+                }
 
-            //     if (plan.interest_type == '1') {
-            //         modal.find('.interestDetails').html(`<strong> Interest: ${interestAmount}% </strong>`);
-            //     } else {
-            //         modal.find('.interestDetails').html(
-            //             `<strong> Interest: ${interestAmount} ${currency}  </strong>`);
-            //     }
+                if (plan.interest_type == '1') {
+                    modal.find('.interestDetails').html(`<strong> Interest: ${interestAmount}% </strong>`);
+                } else {
+                    modal.find('.interestDetails').html(
+                        `<strong> Interest: ${interestAmount} ${currency}  </strong>`);
+                }
 
-            //     if (plan.lifetime == '0') {
-            //         modal.find('.interestValidity').html(
-            //             `<strong>  Every ${plan.time_setting.time} hours for ${plan.repeat_time} times</strong>`
-            //         );
-            //     } else {
-            //         modal.find('.interestValidity').html(
-            //             `<strong>  Every ${plan.time_setting.time} hours for life time </strong>`);
-            //     }
-            //     modal.modal("show");
-            // });
+                if (plan.lifetime == '0') {
+                    modal.find('.interestValidity').html(
+                        `<strong>  Every ${plan.time_setting.time} hours for ${plan.repeat_time} times</strong>`
+                    );
+                } else {
+                    modal.find('.interestValidity').html(
+                        `<strong>  Every ${plan.time_setting.time} hours for life time </strong>`);
+                }
+                modal.modal("show");
+            });
 
             $('[name=amount]').on('input', function() {
                 $('[name=wallet_type]').trigger('change');

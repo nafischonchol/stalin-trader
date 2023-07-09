04ac83d6b1d706ac0942c7791b4e96313d89b4be
Nafis Chonchol <nafisislam161@gmail.com>
2023-07-10 01:23:35 +0600

    add font aweoms

--- resources/views/templates/invester/layouts/app.blade.php
+++ resources/views/templates/invester/layouts/app.blade.php
@@ -26,6 +26,10 @@
     <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/lib/magnific-popup.css') }}">
     <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/lib/apexcharts.css') }}">
 
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    
     <!-- Main css -->
     <link rel="stylesheet" href="{{ asset($activeTemplateTrue . 'css/main.css') }}">
 

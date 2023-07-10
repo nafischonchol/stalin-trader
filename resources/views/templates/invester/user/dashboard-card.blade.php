341f9ab636415c83ea4a367ba4a8fbbf909f1d08
Nafis Chonchol <nafisislam161@gmail.com>
2023-07-11 00:59:29 +0600

    show card

--- resources/views/templates/invester/user/dashboard-card.blade.php
+++ resources/views/templates/invester/user/dashboard-card.blade.php
@@ -1,16 +1,68 @@
-<div class="row g-3 mt-4">
-    <div class="col-md-4">
+<div class="row g-3">
+    <div class="col-md-3">
         <div class="card mb-3 dashboard-card">
             <div class="dashboard-widget dashboard-widget-card">
-                <div class="icon">
-                    Hello
-                </div>
-                <div class="info">
-                    <div class="d-flex justify-content-between">
-                        <h5 class="text-secondary">@lang('Successful Deposits')</h5>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="dashboard-icon">
+                        <span class="material-symbols-outlined">
+                            monitoring
+                        </span>
                     </div>
-                    <h3 class="text--secondary my-4">{{ showAmount($successfulDeposits) }}
-                        {{ $general->cur_text }}</h3>
+                    <div class="info">
+                        <div>
+                            <p style="color:#fff; font-size:10px">@lang('Successful Deposits')</p>
+                        </div>
+                        <div>
+                            <p class="my-4" style="color:#fff; font-size:15px">{{ showAmount($successfulDeposits) }}
+                                {{ $general->cur_text }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card mb-3 dashboard-card">
+            <div class="dashboard-widget dashboard-widget-card">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="dashboard-icon">
+                        <span class="material-symbols-outlined">
+                            monitoring
+                        </span>
+                    </div>
+                    <div class="info">
+                        <div>
+                            <p style="color:#fff; font-size:10px">@lang('Successful Withdrawals')</p>
+                        </div>
+                        <div>
+                            <p class="my-4" style="color:#fff; font-size:15px">
+                                {{ showAmount($successfulWithdrawals) }} {{ $general->cur_text }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3">
+        <div class="card mb-3 dashboard-card">
+            <div class="dashboard-widget dashboard-widget-card">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="dashboard-icon">
+                        <span class="material-symbols-outlined">
+                            monitoring
+                        </span>
+                    </div>
+                    <div class="info">
+                        <div>
+                            <p style="color:#fff; font-size:10px">@lang('Total Investments')</p>
+                        </div>
+                        <div>
+                            <p class="my-4" style="color:#fff; font-size:15px">{{ showAmount($invests) }}
+                                {{ $general->cur_text }}
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/templates/invester/user/hudai.blade.php
+++ resources/views/templates/invester/user/hudai.blade.php
@@ -1,9 +1,8 @@
 @extends($activeTemplate . 'layouts.master')
 @section('content')
-    <div style="background-color: #000000 !important">
-        
+
     <div class="dashboard-inner">
-        @include("templates.invester.user.dashboard-card")
+
         @if ($user->deposit_wallet <= 0 && $user->interest_wallet <= 0)
             <div class="alert border border--danger" role="alert">
                 <div class="alert__icon d-flex align-items-center text--danger"><i class="fas fa-exclamation-triangle"></i>
@@ -131,159 +130,48 @@
             </div>
         @endif
 
-        <div class="card mt-4 mb-4">
-            <div class="card-body">
-                <div class="mb-2">
-                    <h5 class="title">@lang('Latest ROI Statistics')</h5>
-                    <p> <small><i>@lang('Here is last 30 days statistics of your ROI (Return on Investment)')</i></small></p>
+        <div class="row g-3 mt-4">
+            <div class="col-lg-4">
+                <div class="dashboard-widget">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="text-secondary">@lang('Successful Deposits')</h5>
+                    </div>
+                    <h3 class="text--secondary my-4">{{ showAmount($successfulDeposits) }} {{ $general->cur_text }}</h3>
+                    <div class="widget-lists">
+                        <div class="row">
+                            <div class="col-4">
+                                <p class="fw-bold">@lang('Submitted')</p>
+                                <span>{{ $general->cur_sym }}{{ showAmount($submittedDeposits) }}</span>
+                            </div>
+                            <div class="col-4">
+                                <p class="fw-bold">@lang('Pending')</p>
+                                <span>{{ $general->cur_sym }}{{ showAmount($pendingDeposits) }}</span>
+                            </div>
+                            <div class="col-4">
+                                <p class="fw-bold">@lang('Rejected')</p>
+                                <span>{{ $general->cur_sym }}{{ showAmount($rejectedDeposits) }}</span>
+                            </div>
+                        </div>
+                        <hr>
+                        <p><small><i>@lang('You\'ve requested to deposit') {{ $general->cur_sym }}{{ showAmount($requestedDeposits) }}.
+                                    @lang('Where') {{ $general->cur_sym }}{{ showAmount($initiatedDeposits) }}
+                                    @lang('is just initiated but not submitted.')</i></small></p>
+                    </div>
                 </div>
-                <div id="chart"></div>
             </div>
+            <div class="col-lg-4">
+                <div class="dashboard-widget">
+                    <div class="d-flex justify-content-between">
+                        <h5 class="text-secondary">@lang('Successful Withdrawals')</h5>
+                    </div>
+                    <h3 class="text--secondary my-4">{{ showAmount($successfulWithdrawals) }} {{ $general->cur_text }}
+                    </h3>
+                
+                </div>
+            </div>
+          
         </div>
     </div>
 
-</div>
-
 @endsection
 
-{{-- this tree maybe not required fo referal --}}
-@push('style')
-    <link href="{{ asset('assets/global/css/jquery.treeView.css') }}" rel="stylesheet" type="text/css">
-@endpush
-
-
-@push('script')
-    {{-- this tree maybe not required --}}
-    <script src="{{ asset('assets/global/js/jquery.treeView.js') }}"></script>
-    <script>
-        (function($) {
-            "use strict"
-            $('.treeview').treeView();
-            $('.copyBoard').click(function() {
-                var copyText = document.getElementsByClassName("copyURL");
-                copyText = copyText[0];
-                copyText.select();
-                copyText.setSelectionRange(0, 99999);
-
-                /*For mobile devices*/
-                document.execCommand("copy");
-                $('.copyText').text('Copied');
-                setTimeout(() => {
-                    $('.copyText').text('Copy');
-                }, 2000);
-            });
-        })(jQuery);
-    </script>
-    {{-- end referal tree --}}
-
-    <script src="{{ asset($activeTemplateTrue . '/js/lib/apexcharts.min.js') }}"></script>
-
-    <script>
-        // apex-line chart
-        var options = {
-            chart: {
-                height: 350,
-                type: "area",
-                toolbar: {
-                    show: false
-                },
-                dropShadow: {
-                    enabled: true,
-                    enabledSeries: [0],
-                    top: -2,
-                    left: 0,
-                    blur: 10,
-                    opacity: 0.08,
-                },
-                animations: {
-                    enabled: true,
-                    easing: 'linear',
-                    dynamicAnimation: {
-                        speed: 1000
-                    }
-                },
-            },
-            dataLabels: {
-                enabled: false
-            },
-            series: [{
-                name: "Price",
-                data: [
-                    @foreach ($chartData as $cData)
-                        {{ getAmount($cData->amount) }},
-                    @endforeach
-
-                ]
-            }],
-            fill: {
-                type: "gradient",
-                colors: ['#4c7de6', '#4c7de6', '#4c7de6'],
-                gradient: {
-                    shadeIntensity: 1,
-                    opacityFrom: 0.6,
-                    opacityTo: 0.9,
-                    stops: [0, 90, 100]
-                }
-            },
-            xaxis: {
-                title: "Value",
-                categories: [
-                    @foreach ($chartData as $cData)
-                        "{{ Carbon\Carbon::parse($cData->date)->format('d F') }}",
-                    @endforeach
-                ]
-            },
-            grid: {
-                padding: {
-                    left: 5,
-                    right: 5
-                },
-                xaxis: {
-                    lines: {
-                        show: false
-                    }
-                },
-                yaxis: {
-                    lines: {
-                        show: false
-                    }
-                },
-            },
-        };
-
-        var chart = new ApexCharts(document.querySelector("#chart"), options);
-
-        chart.render();
-
-        @if ($isHoliday)
-            function createCountDown(elementId, sec) {
-                var tms = sec;
-                var x = setInterval(function() {
-                    var distance = tms * 1000;
-                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-                    var days = `<span>${days}d</span>`;
-                    var hours = `<span>${hours}h</span>`;
-                    var minutes = `<span>${minutes}m</span>`;
-                    var seconds = `<span>${seconds}s</span>`;
-                    document.getElementById(elementId).innerHTML = days + ' ' + hours + " " + minutes + " " +
-                        seconds;
-                    if (distance < 0) {
-                        clearInterval(x);
-                        document.getElementById(elementId).innerHTML = "COMPLETE";
-                    }
-                    tms--;
-                }, 1000);
-            }
-
-            createCountDown('counter', {{ \Carbon\Carbon::parse($nextWorkingDay)->diffInSeconds() }});
-        @endif
-
-        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-            return new bootstrap.Tooltip(tooltipTriggerEl)
-        })
-    </script>
-@endpush
